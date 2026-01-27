846d4b0d8fdb7959c266f964f0aac8116e9e1762
ssolovev <soloovushka@ya.ru>
2026-01-27 16:29:12 +0300

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text
+        style={styles.text}
+      >Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#25292e',
+  },
+  text: {
+    color: '#fff'
+  }
+})
