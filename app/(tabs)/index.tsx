4d47a0c59bcc4b2bf50d4c461a273fb6d7a3978e
ssolovev <soloovushka@ya.ru>
2026-02-01 23:51:00 +0300

    add Home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,20 @@
 import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+
+const PlaceholderImage = require("@/assets/images/background-image.png")
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text
-        style={styles.text}
-      >Home screen</Text>
-      <Link href={'/about'} style={styles.button}>Go to About</Link>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -18,16 +23,14 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#25292e',
+    alignItems: 'center',
   },
-  text: {
-    color: '#fff'
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 })
