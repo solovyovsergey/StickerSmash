f4752c601f3a8d8b217cca8b3298cc35df05ed51
ssolovev <soloovushka@ya.ru>
2026-02-04 13:51:10 +0300

    add image picker

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,9 +3,10 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 type Props = {
     label: string;
-    theme?: 'primary'
+    theme?: 'primary',
+    onPress?: () => void
 }
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
     if (theme === 'primary') {
         return (
             <View
@@ -15,7 +16,7 @@ export default function Button({ label, theme }: Props) {
                 ]}>
                 <Pressable
                     style={[styles.button, { backgroundColor: '#fff' }]}
-                    onPress={() => alert('You pressed a button.')}>
+                    onPress={onPress}>
                     <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
                     <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
                 </Pressable>
@@ -26,7 +27,7 @@ export default function Button({ label, theme }: Props) {
 
     return (
         <View style={styles.buttonContainer}>
-            <Pressable style={styles.button} onPress={() => alert('Вы нажали кнопку')}>
+            <Pressable style={styles.button} onPress={onPress}>
                 <Text style={styles.buttonLabel}>{label}</Text>
             </Pressable>
         </View>
--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -2,11 +2,14 @@ import { Image } from "expo-image"
 import { ImageSourcePropType, StyleSheet } from "react-native"
 
 type Props = {
-    imgSource: ImageSourcePropType
+    imgSource: ImageSourcePropType,
+    selectedImage?: string,
 }
 
-export default function ImageViewer({ imgSource }: Props) {
-    return <Image source={imgSource} style={styles.image} />
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+    const imageSource = selectedImage ? { uri: selectedImage } : imgSource
+
+    return <Image source={imageSource} style={styles.image} />
 }
 
 const styles = StyleSheet.create({
